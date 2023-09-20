51912291afbcf03f14c0d11b11d7341078e35527
tomcat <tommypo26@gmail.com>
2023-09-20 15:46:16 +0300

    misc

--- src/shared/lib/get-initials.ts
+++ src/shared/lib/get-initials.ts
@@ -1,8 +1,14 @@
-export function getInitials(inputString: string): string {
-  return (
-    inputString
-      ?.split(' ')
-      .map(word => word[0].toUpperCase())
-      .join('') || ''
-  ).toUpperCase();
+
+
+export function getInitials(inputString: string): string  {
+  if (!inputString) {
+    return '';
+  }
+
+  const newStrings = inputString.split(' ')
+
+  if (newStrings.length > 1) {
+    return newStrings[0].slice(0, 1) + newStrings[1].slice(0, 1);
+  }
+  return newStrings[0][0];
 }
